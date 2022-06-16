ae778b37aa578c7c005a0b7ddd474cb9c119482d
Anton V. Kirilchik <kosmonaffft@gmail.com>
2022-06-16 19:28:10 +0300

    Add comments...

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,3 +1,4 @@
+// I'm comment.
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    // I'm comment.
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
